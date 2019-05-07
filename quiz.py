68717437081bb7b5642ab81e332180eb058d95cd
zoyazee <47854404+zoyazee@users.noreply.github.com>
2019-05-07 19:22:43 +0300

    Add files via upload
    class Quiz

---
+++ quiz.py
@@ -0,0 +1,52 @@
+def tens(m):
+    a=[]
+    for x in a*10:
+        m.append(x)
+    return m
+
+def squares(z):
+    b=[]
+    h=[x*x for x in b]
+    return h
+
+def sorted(d):
+    list=[]
+    list2=[]
+    sort()
+    return c
+
+def range_sum(n):
+    sum=[]
+    a=sum()
+    for z in a:
+        return a
+
+def largest(f):
+    list=[]
+    a=reverse()
+    for m in a:
+        return m
+
+def smallest(k):
+    list=[]
+    r=sort()
+    for d in r:
+        return r
+
+def my_modulus(z):
+    z=dict()
+    for x in range():
+        z[x]=x%3
+        return z
+
+def student(students):
+    student1={"balance":4000,"name":"Joy"}
+    student2={"balance":1000,"name":"Zoya"}
+    student3={"balance":2000,"name":"Wanja"}
+    student4={"balance":5000,"name":"Zee"}
+    for student in students:
+        return student
+
+
+
+
